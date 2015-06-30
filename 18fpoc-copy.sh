bd80dc2f75b38ab89bc55c8534de5f4ccdbd7a68
Harjit Singh <Harjit@teslagovernment.com>
2015-06-30 12:40:54 -0400

    Fixing condition

--- 18fpoc-copy.sh
+++ 18fpoc-copy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -ev
-
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
-    export DEPLOY_HOST=558aaeb2500446c6d300019c@18fpoc-iworksfda.rhcloud.com
+    DEPLOY_HOST=558aaeb2500446c6d300019c@18fpoc-iworksfda.rhcloud.com
+else
+    DEPLOY_HOST=558b6aed5973ca5b1b00004f@18fpocdev-iworksfda.rhcloud.com
 fi
-
-sshpass scp -i ~/id_rsa_dpl webapps/ROOT.war $DEPLOY_HOST:$DEPLOY_PATH
 
 
