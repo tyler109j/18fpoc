3bbb75c5e5d1e3ef056c7fbe529b4b5b8ccde6f9
Harjit Singh <Harjit@teslagovernment.com>
2015-06-30 20:40:49 -0400

    fixed script

--- 18fpoc-copy.sh
+++ 18fpoc-copy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ev
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
-    DEPLOY_HOST=558aaeb2500446c6d300019c@18fpoc-iworksfda.rhcloud.com
+    export DEPLOY_HOST=558aaeb2500446c6d300019c@18fpoc-iworksfda.rhcloud.com
 else
-    DEPLOY_HOST=558b6aed5973ca5b1b00004f@18fpocdev-iworksfda.rhcloud.com
+   export DEPLOY_HOST=558b6aed5973ca5b1b00004f@18fpocdev-iworksfda.rhcloud.com
 fi
 
 
